924ba40cf54dcf0522f505d64af703835f73bc1b
Sonu Singh <sonu@fintechlabs.in>
2017-12-16 13:22:36 +0530

    Resolve changing index.html

---
+++ src/main/groovy/assets/js/custom.js
@@ -0,0 +1,125 @@
+$(document).ready(function () {
+    $.ajax({
+        url: "",
+        type: "post",
+        data: "",
+        contentType: "",
+        success: function (data) {
+            alert(data);
+        },
+        error: function (data) {
+            alert("Error Message")
+        }
+    });
+    $.ajax({
+        url: "",
+        type: "POST",
+        data: "",
+        contentType: "",
+        success: function () {
+
+        },
+        error: function () {
+
+        }
+
+    });
+    $.ajax({
+        url: "",
+        type: "POST",
+        data: "",
+        contentType: "",
+        success: function () {
+
+        },
+        error: function () {
+
+        }
+    });
+    $.ajax({
+        url: "",
+        type: "POST",
+        data: "",
+        contentType: "",
+        success: function () {
+
+        },
+        error: function () {
+
+        }
+    });
+    $.ajax({
+        url: "",
+        type: "POST",
+        data: "",
+        contentType: "",
+        success: function () {
+
+        },
+        error: function () {
+
+        }
+    });
+    $.ajax({
+        url: "",
+        type: "POST",
+        data: "",
+        contentType: "",
+        success: function () {
+
+        },
+        error: function () {
+
+        }
+    });
+    $.ajax({
+        url: "",
+        type: "POST",
+        data: "",
+        contentType: "",
+        success: function () {
+
+        },
+        error: function () {
+
+        }
+    });
+    $.ajax({
+        url: "",
+        type: "POST",
+        data: "",
+        contentType: "",
+        success: function () {
+
+        },
+        error: function () {
+
+        }
+    });
+    $.ajax({
+        url: "",
+        type: "POST",
+        data: "",
+        contentType: "",
+        success: function () {
+
+        },
+        error: function () {
+
+        }
+    });
+    $.ajax({
+        url: "",
+        type: "POST",
+        data: "",
+        contentType: "",
+        success: function () {
+
+        },
+        error: function () {
+
+        }
+    });
+
+
+});
