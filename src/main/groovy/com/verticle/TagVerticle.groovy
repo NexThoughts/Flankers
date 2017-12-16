88b137d0376fd8ef4e87fb6a1cce12562bd5a2b3
Sonu Singh <sonu@fintechlabs.in>
2017-12-16 19:24:07 +0530

    Tag Crud

--- src/main/groovy/com/verticle/TagVerticle.groovy
+++ src/main/groovy/com/verticle/TagVerticle.groovy
@@ -1,8 +1,11 @@
 package com.verticle
 
 import com.Config
+import com.model.Tag
+import com.model.User
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.http.HttpMethod
+import io.vertx.core.json.Json
 import io.vertx.core.json.JsonObject
 import io.vertx.ext.mongo.MongoClient
 import io.vertx.ext.web.Router
@@ -18,7 +21,7 @@ class TagVerticle extends AbstractVerticle {
         mongoClient = MongoClient.createShared(vertx, new JsonObject().put("db_name", Config.dbName))
 
         Router router = Router.router(vertx)
-
+        println("********Start************")
         router.route().handler(CorsHandler.create("*")
                 .allowedMethod(HttpMethod.POST)
                 .allowedMethod(HttpMethod.DELETE)
@@ -27,29 +30,56 @@ class TagVerticle extends AbstractVerticle {
                 .allowedHeader("X-PINGARUNER")
                 .allowedHeader("Content-Type"))
 
-
+        println("********Start************")
+        createRouteForTag(router)
         vertx.createHttpServer().requestHandler(router.&accept).listen(8085)
-    }
-
-    void createRouteForUser(Router router) {
-        router.get("/users").handler(this.&fetchAllUsers)
-        router.route("/users*").handler(BodyHandler.create())
-        router.post("/users").handler(this.&addUser)
-        router.get("/users/count").handler(this.&fetchUsersCount)
-        router.get("/users/:id").handler(this.&fetchSingleUser)
-        router.put("/users/:id").handler(this.&updateUser)
-        router.delete("/users/:id").handler(this.&deleteUser)
-    }
-
-    void deleteUser(RoutingContext routingContext) {
 
     }
 
-    void addUser(RoutingContext routingContext) {
+    void createRouteForTag(Router router) {
+        router.route("/tags*").handler(BodyHandler.create())
+        router.get("/tags").handler(this.&fetchAllUsers)
+        router.post("/addTag").handler(this.&addTag)
+        router.get("/tags/count").handler(this.&fetchUsersCount)
+        router.get("/tags/:id").handler(this.&fetchSingleUser)
+        router.put("/tags/:id").handler(this.&updateUser)
+        router.delete("/tags/:id").handler(this.&deleteTags)
+    }
+
+    void deleteTags(RoutingContext routingContext) {
+        println("**************IN Tag Delete Method *****************")
+        mongoClient.removeOne("tag", new JsonObject().put("uuid", routingContext.request().getParam("id")), { lookup ->
+            if (lookup.failed()) {
+                routingContext.fail(lookup.cause())
+                return
+            }
+
+            routingContext.response().setStatusCode(204)
+            routingContext.response().end()
+        })
+
+    }
+
+    void addTag(RoutingContext routingContext) {
+        println("**************IN Tag Add Method *****************")
+        Tag tag = Json.decodeValue(routingContext.getBodyAsString(), Tag.class)
+        println("tagggg" + tag)
+        println("tagggg" + tag)
+        println("tagggg" + tag)
+
+        mongoClient.insert("tag", tag.jsonObject(), { handeller ->
+            if (handeller.failed()) {
+                routingContext.fail(handeller.cause())
+            }
+            tag.id = handeller.result()
+            routingContext.response().setStatusCode(201).putHeader("content-type", "application/json; charset=utf-8")
+                    .end(Json.encodePrettily(tag))
+        })
 
     }
 
     void fetchUsersCount(RoutingContext routingContext) {
+
 
     }
 
